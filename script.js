5d872ec487dfb403a8c525d48f9d9c9237bdc785
Kota Fujimoto <49606319+kota-may478@users.noreply.github.com>
2023-10-07 20:57:53 +0200

    Update

--- script.js
+++ script.js
@@ -1,35 +1,8 @@
-// Function to navigate to a specific section on the page
-function navigateTo(sectionId, adjustForHeader = false) {
-    // Get the target section by its ID
-    const section = document.getElementById(sectionId);
-    if (section) {
-        // Get the height of the header
-        const headerHeight = document.querySelector("header").offsetHeight;
-
-        // Calculate the target scroll position
-        let scrollToPosition = section.offsetTop-20;
-        if (adjustForHeader) {
-            scrollToPosition -= headerHeight; // Adjust for header height if needed
-        }
-
-        // Scroll to the target position
-        window.scrollTo({
-            top: scrollToPosition,
-            behavior: "smooth" // Smooth scrolling effect
-        });
-    }
-}
-
 let flag_open = 0; 
-
-// Function to navigate to a section and close the slide menu
-function navigateAndClose(sectionId) {
-    if (flag_open === 1) {
-        navigateTo(sectionId, true); // Navigate to the section immediately
-        closeSlideMenu(); // Close the slide menu and start fading out the overlay
-    }
-}
-
+let startX = 0; // Initial touch X-coordinate
+let currentX = 0; // Current touch X-coordinate
+const slideMenu = document.getElementById('slide-menu');
+const closeBtn = document.getElementById('slide-menu-close');
 const isTouchDevice = 'ontouchstart' in window;
 
 // Event listener for when the DOM is fully loaded
@@ -69,14 +42,8 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     const hamburgerIcon = document.getElementById('hamburger-icon');
+    hamburgerIcon.addEventListener('click', toggleMenu);
 
-    function toggleMenu() {
-        // const menu = document.getElementById('slide-menu');
-        if (flag_open === 0) {
-            flag_open === 0.5
-            openSlideMenu();
-        }
-    }
     
     // if (isTouchDevice) {
     //     hamburgerIcon.addEventListener('touchend', function(event) {
@@ -84,10 +51,83 @@ document.addEventListener('DOMContentLoaded', function() {
     //         toggleMenu();
     //     });
     // } else {
-    hamburgerIcon.addEventListener('click', toggleMenu);
     // }
 
 });
+
+slideMenu.addEventListener('touchstart', function(event) {
+    startX = event.touches[0].clientX;
+    currentX = slideMenu.style.right ? parseInt(slideMenu.style.right) : -280;
+    currentX_Btn = closeBtn.style.left ? parseInt(closeBtn.style.left) : -90;
+}, false);
+
+slideMenu.addEventListener('touchmove', function(event) {
+    let touchX = event.touches[0].clientX;
+    let diffX = startX - touchX;
+    let newRight = currentX + 4*diffX;
+    let newLeft = currentX_Btn;
+
+    // Restrict the position of the slide menu
+    if (newRight < -280) {
+        newRight = -280;
+    }
+    if (newRight > 0) newRight = 0;
+    if (newLeft > 0) newLeft = 0;
+    // if (newLeft > 40) newLeft = 40;
+
+    slideMenu.style.right = newRight + 'px';
+    closeBtn.style.left = newLeft + 'px';
+}, false);
+
+slideMenu.addEventListener('touchend', function(event) {
+    // Decide whether to open or close the slide menu based on its position
+    if (parseInt(slideMenu.style.right) < -50) { // center: -115
+        closeSlideMenu();
+    } else {
+        openSlideMenu();
+    }
+}, false);
+
+// Event listener for clicking the close button of the slide menu
+document.getElementById('slide-menu-close').addEventListener('click', closeSlideMenu);
+
+// Function to navigate to a specific section on the page
+function navigateTo(sectionId, adjustForHeader = false) {
+    // Get the target section by its ID
+    const section = document.getElementById(sectionId);
+    if (section) {
+        // Get the height of the header
+        const headerHeight = document.querySelector("header").offsetHeight;
+
+        // Calculate the target scroll position
+        let scrollToPosition = section.offsetTop-20;
+        if (adjustForHeader) {
+            scrollToPosition -= headerHeight; // Adjust for header height if needed
+        }
+
+        // Scroll to the target position
+        window.scrollTo({
+            top: scrollToPosition,
+            behavior: "smooth" // Smooth scrolling effect
+        });
+    }
+}
+
+// Function to navigate to a section and close the slide menu
+function navigateAndClose(sectionId) {
+    if (flag_open === 1) {
+        navigateTo(sectionId, true); // Navigate to the section immediately
+        closeSlideMenu(); // Close the slide menu and start fading out the overlay
+    }
+}
+
+function toggleMenu() {
+    // const menu = document.getElementById('slide-menu');
+    if (flag_open === 0) {
+        flag_open = 0.5;
+        openSlideMenu();
+    }
+}
 
 // Function to open the slide menu
 function openSlideMenu() {
@@ -140,50 +180,6 @@ function closeSlideMenu(callback) {
 //     }
 //     flag_open = 1;
 // });
-
-// Event listeners for touch interactions with the slide menu
-let startX = 0; // Initial touch X-coordinate
-let currentX = 0; // Current touch X-coordinate
-const slideMenu = document.getElementById('slide-menu');
-const closeBtn = document.getElementById('slide-menu-close');
-
-slideMenu.addEventListener('touchstart', function(event) {
-    startX = event.touches[0].clientX;
-    currentX = slideMenu.style.right ? parseInt(slideMenu.style.right) : -280;
-    currentX_Btn = closeBtn.style.left ? parseInt(closeBtn.style.left) : -90;
-}, false);
-
-slideMenu.addEventListener('touchmove', function(event) {
-    let touchX = event.touches[0].clientX;
-    let diffX = startX - touchX;
-    let newRight = currentX + 4*diffX;
-    let newLeft = currentX_Btn;
-
-    // Restrict the position of the slide menu
-    if (newRight < -280) {
-        newRight = -280;
-    }
-    if (newRight > 0) newRight = 0;
-    if (newLeft > 0) newLeft = 0;
-    // if (newLeft > 40) newLeft = 40;
-
-    slideMenu.style.right = newRight + 'px';
-    closeBtn.style.left = newLeft + 'px';
-}, false);
-
-slideMenu.addEventListener('touchend', function(event) {
-    // Decide whether to open or close the slide menu based on its position
-    if (parseInt(slideMenu.style.right) < -50) { // center: -115
-        closeSlideMenu();
-    } else {
-        openSlideMenu();
-    }
-}, false);
-
-if (flag_open === 1) {
-    // Event listener for clicking the close button of the slide menu
-    document.getElementById('slide-menu-close').addEventListener('click', closeSlideMenu);
-}
 
 // Function to toggle between Japanese and English pages
 function toggleLanguage() {
